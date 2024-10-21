cfcbd2256667dab21bbcb343e1a59a7b5b44ca92
xNorthernWolf <xnorthernwolf@gmail.com>
2024-10-21 15:24:03 +0200

    1.0.1 version

--- main.py
+++ main.py
@@ -48,8 +48,14 @@ while True:
         lps_list = []
         file_names = os.listdir(lps_have)
         for file in file_names:
-            lps_list.append(file)
-        print(lps_list)
+            start_index = file.find('(')
+            end_index = file.find(')')
+            if start_index != -1 and end_index != -1 and end_index > start_index:
+                  extracted_string = file[start_index + 1:end_index]  # Extract the string between parentheses
+            if extracted_string:  # Check if the extracted string is not empty
+                lps_list.append(extracted_string)  # Only append non-empty strings
+        print(", ".join(lps_list))
+        break
 
     elif user_input == 'download' or user_input == 'd':
         ps_download = input('Which gen would you like to download?\n').lower()
