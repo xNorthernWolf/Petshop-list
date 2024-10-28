8c01c06b70d49e5c97bdd748c34b3d6afc66341c
xNorthernWolf <xnorthernwolf@gmail.com>
2024-10-28 15:24:37 +0100

    finallY works

--- download_lps.py
+++ download_lps.py
@@ -10,11 +10,16 @@ import os
 
 def download(n):
 # Gets html and makes it human-readable. Filters the div data-item from the html (The square that stores the images and the petshop info). Establishes a directory to store the images and a set to avoid downloading duplicates. 
-    url = f'https://lpsmerch.com/g{n}/all/'
+
+    url = rf'https://lpsmerch.com/g{n}/all/'
+    print(url)
     request = requests.get(url)
     soup = bs4.BeautifulSoup(request.text, 'html.parser')
-    filtered_soup = soup.find_all('div', class_='data-item')
-    image_directory = r'C:\Users\xNort\Desktop\Cosas varias\DevOps\Python\Petshop-list\lps_all'
+    if n == '7' or n == '6':
+        filtered_soup = soup.find_all('div', class_='data-item-long')
+    else:
+        filtered_soup = soup.find_all('div', class_='data-item')
+    image_directory = rf'C:\Users\xNort\Desktop\Cosas varias\DevOps\Python\Petshop-list\lps_all\gen_{n}'
     downloaded_items = set()
 
 
@@ -26,13 +31,13 @@ def download(n):
         bottom_div = element.find_next('div', class_='data-bottom')
         top_div = element.find_next('div', class_='data-top')
 
+
         # Retrieves the name and number of each petshop
         if bottom_div:
             a_tags_bottom = bottom_div.find('a')
             lps_name = a_tags_bottom.get_text(strip=True)
-            number = ''
             element_text = element.get_text(strip=True)
-            match = re.search(r'\((.*?)\)', element_text)
+            match = re.search(r'\(([^)]*#.*?)\)', element_text)
             if match:
                 lps_number = match.group()
             lps_name_num = lps_number + ' ' + lps_name
