05f7c64bd2a9bfa92d670aa1bd385037991e7a83
Dante <dante@Dantes-MacBook-Air.local>
2025-02-06 13:50:45 +0100

    Update

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-lps_path = '/Users/dante/Desktop/Petshop-list'
+# DO NOT DELETE the 'r' in front of the path.
+# Just edit what's inside the quotation marks
+lps_path = r'/Users/dante/Desktop/Petshop-list'
